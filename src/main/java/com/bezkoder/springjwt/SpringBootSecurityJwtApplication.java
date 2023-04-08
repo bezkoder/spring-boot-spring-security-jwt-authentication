5f2ae8c4a4b4ae2fba95a9b8f5561912c1402ebe
Ramazan Sakin <ramazansakin63@gmail.com>
2023-04-08 10:33:40 +0300

    pre-defined roles initialized not to get "Role not found" error

--- src/main/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplication.java
+++ src/main/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplication.java
@@ -1,13 +1,35 @@
 package com.bezkoder.springjwt;
 
+import com.bezkoder.springjwt.models.ERole;
+import com.bezkoder.springjwt.models.Role;
+import com.bezkoder.springjwt.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.Optional;
+
 @SpringBootApplication
 public class SpringBootSecurityJwtApplication {
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        // Create roles if they don't exist
+        for (ERole role : ERole.values()) {
+            Optional<Role> optionalRole = roleRepository.findByName(role);
+            if (!optionalRole.isPresent()) {
+                Role newRole = new Role();
+                newRole.setName(role);
+                roleRepository.save(newRole);
+            }
+        }
+    }
 }
