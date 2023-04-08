5f6d3c495814e4894bb8628ed0fd1a669fb48101
Ramazan Sakin <ramazansakin63@gmail.com>
2023-04-08 10:33:15 +0300

    SignupRequest required fields defined

--- src/main/java/com/bezkoder/springjwt/payload/request/SignupRequest.java
+++ src/main/java/com/bezkoder/springjwt/payload/request/SignupRequest.java
@@ -3,21 +3,25 @@ package com.bezkoder.springjwt.payload.request;
 import lombok.Value;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Value
 public class SignupRequest {
 
+    @NotBlank(message = "username is required")
     @Size(min = 3, max = 20)
     String username;
 
+    @NotBlank(message = "email is required")
     @Size(max = 50)
     @Email
     String email;
 
     Set<String> roles;
 
+    @NotBlank(message = "password is required")
     @Size(min = 6, max = 40)
     String password;
 
