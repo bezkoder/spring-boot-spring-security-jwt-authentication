81632febf8880df7b4c524da60bef0864d24d840
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-29 16:19:01 +0300

    Add attributes and colum names to channel table

--- src/main/java/com/blaquesystems/backend/models/Channel.java
+++ src/main/java/com/blaquesystems/backend/models/Channel.java
@@ -1,4 +1,33 @@
 package com.blaquesystems.backend.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "channels")
 public class Channel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 20)
+    private String name;
+
+    @Size(max = 300)
+    private String description;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private Date updatedAt;
 }
