c88f97e1eff15caf20b8f12e93801627b7890fc5
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-29 16:44:48 +0300

    Generate constructors and getters and setter metthods

--- src/main/java/com/blaquesystems/backend/models/Channel.java
+++ src/main/java/com/blaquesystems/backend/models/Channel.java
@@ -30,4 +30,53 @@ public class Channel {
     @Column(name = "updated_at", nullable = false)
     @LastModifiedDate
     private Date updatedAt;
+
+    public Channel(){}
+
+    public Channel(String name, String description, Date createdAt, Date updatedAt) {
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
