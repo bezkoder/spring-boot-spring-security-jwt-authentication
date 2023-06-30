30ac690ac78f39651fa98518fe70a557bbb5ce6d
Vincent-kipngeno <kipngenovi2001@gmail.com>
2023-06-30 13:10:10 +0300

    Add SmsUtils with the required app properties

---
+++ src/main/java/com/blaquesystems/backend/utils/SmsUtils.java
@@ -0,0 +1,28 @@
+package com.blaquesystems.backend.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SmsUtils {
+
+    @Value("${bezkoder.app.sms.url}")
+    private String url;
+
+    @Value("${bezkoder.app.sms.apiClientID}")
+    private int apiClientId;
+
+    @Value("${bezkoder.app.sms.key}")
+    private String key;
+
+    @Value("${bezkoder.app.sms.secret}")
+    private String secret;
+
+    @Value("${bezkoder.app.sms.MSISDN}")
+    private Long msisdn;
+
+    public String getFullSmsUrl(String message, int serviceId) {
+        return String.format("%sapiClientID=%d&key=%s&secret=%s&txtMessage=%s&MSISDN=%d&serviceID=%d",
+                url, apiClientId, key, secret, message, msisdn, serviceId);
+    }
+}
