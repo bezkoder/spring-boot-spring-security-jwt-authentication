1c2c3ef64743e0d04f48575c56e372e424eb8e2b
Don <munenederrick862@gmail.com>
2023-07-05 03:14:51 +0300

    WIP: added the @Repository annotation

--- src/main/java/com/blaquesystems/backend/repository/BusinessRepository.java
+++ src/main/java/com/blaquesystems/backend/repository/BusinessRepository.java
@@ -2,7 +2,9 @@ package com.blaquesystems.backend.repository;
 
 import com.blaquesystems.backend.models.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BusinessRepository extends JpaRepository<Business, Long> {
     Boolean existsByEmail(String email);
     Boolean existsByName(String name);
